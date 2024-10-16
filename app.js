f82815bfc933f7084eb089c6b66e296b226efb78
Asthere0n <cristiansanchezsonido@gmail.com>
2024-10-16 18:23:49 +0100

    Added system who will only let you have 3 tics on the board, possibly making this game go infite.

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const HeaderTitle = document.getElementById('title')
 const Squares = document.querySelectorAll('.square')
 const NewGameButton = document.getElementById('newGameButton')
 let currentPlayer, newGame
+const emptyBoard = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
-const emptyBoard = [[0,0,0],[0,0,0],[0,0,0]]
+console.log(Squares)
 
 
 //Player class
@@ -22,6 +23,9 @@ class Player {
         console.log(`is ${this.name}'s turn NOW!`)
         pageBackground.style.backgroundColor = this.color
         HeaderTitle.innerHTML = this.name
+        if (this.previousMoves.length === 3){
+            this.previousMoves[0].classList.add('forgetting')
+        }
     }
 }
 
@@ -29,19 +33,30 @@ class Player {
 class TicTacToe {
     Player1 = new Player('Player1', 'red', 'X');
     Player2 = new Player('Player2', 'blue', 'O');
-    board = Squares
+    board = [
+        [document.getElementById('A1'), document.getElementById('A2'), document.getElementById('A3')],
+        [document.getElementById('B1'), document.getElementById('B2'), document.getElementById('B3')],
+        [document.getElementById('C1'), document.getElementById('C2'), document.getElementById('C3')]]
 
-
+    //Drawing the board in screen
+    drawBoard () {
+    this.board.forEach()
+    }
 
     //Checking for Win conditions
-    checkVictory(){
+    checkVictory() {
         console.log('You Won!')
     }
 
     //End turn
-    endTurn(){
+    endTurn() {
         this.checkVictory()
-        if (currentPlayer == this.Player1){
+        if (currentPlayer.previousMoves.length === 4){
+            const forgottenSquare = currentPlayer.previousMoves.shift()
+            forgottenSquare.innerHTML = ''
+            forgottenSquare.classList.remove('forgetting')
+        }
+        if (currentPlayer == this.Player1) {
             currentPlayer = this.Player2
             currentPlayer.startTurn()
         } else {
@@ -50,27 +65,26 @@ class TicTacToe {
         }
     }
 
+    //Starting a new game
+    startNewGame() {
+        console.log('New Game started')
+        newGame = new TicTacToe
 
-//Starting a new game
-startNewGame(){
-    console.log('New Game started')
-    newGame = new TicTacToe 
+        //Resetting the board
+        for (let square of newGame.board) {
+            square.innerHTML = ''
+        }
 
-    //Resetting the board
-    for (let square of newGame.board) {
-        square.innerHTML = ''
+        //Defining starting player at random
+        if (Math.random() >= 0.5) {
+            newGame.Player1.startTurn()
+            currentPlayer = newGame.Player1
+        } else {
+            newGame.Player2.startTurn()
+            currentPlayer = newGame.Player2
+        }
+
     }
-
-    //Defining starting player at random
-    if (Math.random() >= 0.5) {
-        newGame.Player1.startTurn()
-        currentPlayer = newGame.Player1
-    } else {
-        newGame.Player2.startTurn()
-        currentPlayer = newGame.Player2
-    }
-    
-}
 }
 
 //Launching the game when pressing New Game Button
@@ -85,8 +99,9 @@ Squares.forEach((square) => {
         //Making moves only if game has started already
         if (square.innerHTML === '' && currentPlayer != undefined) {
             square.innerHTML = currentPlayer.mark
-           currentPlayer.previousMoves.push(square)
-           newGame.endTurn()
+            currentPlayer.previousMoves.push(square)
+            console.log(currentPlayer.previousMoves)
+            newGame.endTurn()
         }
 
     })
