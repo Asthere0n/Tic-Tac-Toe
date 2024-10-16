2f4f81dc41f6e3990b6c745b903b901fb60023b3
Asthere0n <cristiansanchezsonido@gmail.com>
2024-10-16 19:26:00 +0100

    Project completely functional

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 //Globar variables
 const pageBackground = document.getElementById('layout')
-const HeaderTitle = document.getElementById('title')
+const HeaderTitle = document.getElementById('headerTitle')
 const Squares = document.querySelectorAll('.square')
 const NewGameButton = document.getElementById('newGameButton')
 let currentPlayer, newGame
-const emptyBoard = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+let Victory = false
 
-console.log(Squares)
 
 
 //Player class
@@ -21,9 +20,13 @@ class Player {
 
     startTurn() {
         console.log(`is ${this.name}'s turn NOW!`)
+
+        //Indicates whose turn is it
         pageBackground.style.backgroundColor = this.color
         HeaderTitle.innerHTML = this.name
-        if (this.previousMoves.length === 3){
+
+        //Indicates wich square is going to be Forgotten after your next pick
+        if (this.previousMoves.length === 3) {
             this.previousMoves[0].classList.add('forgetting')
         }
     }
@@ -33,33 +36,55 @@ class Player {
 class TicTacToe {
     Player1 = new Player('Player1', 'red', 'X');
     Player2 = new Player('Player2', 'blue', 'O');
-    board = [
-        [document.getElementById('A1'), document.getElementById('A2'), document.getElementById('A3')],
-        [document.getElementById('B1'), document.getElementById('B2'), document.getElementById('B3')],
-        [document.getElementById('C1'), document.getElementById('C2'), document.getElementById('C3')]]
 
-    //Drawing the board in screen
-    drawBoard () {
-    this.board.forEach()
-    }
 
     //Checking for Win conditions
     checkVictory() {
-        console.log('You Won!')
+        const winningPositions = [
+            ['A1', 'A2', 'A3'],
+            ['B1', 'B2', 'B3'],
+            ['C1', 'C2', 'C3'],
+            ['A1', 'B1', 'C1'],
+            ['A2', 'B2', 'C2'],
+            ['A3', 'B3', 'C3'],
+            ['A1', 'B2', 'C3'],
+            ['C1', 'B2', 'A3']
+        ]
+
+
+        for (let position of winningPositions) {
+            if (position.every(id => currentPlayer.previousMoves.some(square => square.id === id))) {
+                return true;
+            }
+        }
+        return false;
     }
 
     //End turn
     endTurn() {
-        this.checkVictory()
-        if (currentPlayer.previousMoves.length === 4){
+        //Removes the forgotten squared
+        if (currentPlayer.previousMoves.length === 4) {
             const forgottenSquare = currentPlayer.previousMoves.shift()
             forgottenSquare.innerHTML = ''
             forgottenSquare.classList.remove('forgetting')
         }
-        if (currentPlayer == this.Player1) {
+
+        //Checks if that was a winning move. 
+        if (this.checkVictory()){
+            Victory = true
+            console.log(`${currentPlayer.name} WON!`)
+            console.log(currentPlayer.previousMoves)
+            currentPlayer.previousMoves.forEach((square)=>{
+                square.classList.add('win')
+            }
+            );
+        }
+
+        // If no-one won yet then start next player Turn
+        if (currentPlayer == this.Player1 && Victory === false) {
             currentPlayer = this.Player2
             currentPlayer.startTurn()
-        } else {
+        } else if (currentPlayer == this.Player2 && Victory === false){
             currentPlayer = this.Player1
             currentPlayer.startTurn()
         }
@@ -68,11 +93,14 @@ class TicTacToe {
     //Starting a new game
     startNewGame() {
         console.log('New Game started')
+        Victory=false
         newGame = new TicTacToe
 
         //Resetting the board
-        for (let square of newGame.board) {
+        for (let square of Squares) {
             square.innerHTML = ''
+            square.classList.remove('forgetting')
+            square.classList.remove('winner')
         }
 
         //Defining starting player at random
@@ -97,7 +125,7 @@ NewGameButton.addEventListener('click', () => {
 Squares.forEach((square) => {
     square.addEventListener('click', () => {
         //Making moves only if game has started already
-        if (square.innerHTML === '' && currentPlayer != undefined) {
+        if (square.innerHTML === '' && currentPlayer != undefined && Victory === false) {
             square.innerHTML = currentPlayer.mark
             currentPlayer.previousMoves.push(square)
             console.log(currentPlayer.previousMoves)
